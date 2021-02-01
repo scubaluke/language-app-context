6cdee832fa2416f436a28958f115257d45bb6957
Luke Gartland <lukegartland@gmail.com>
2021-01-31 19:51:44 -1000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import UserCreate from './UserCreate'
+import { LanguageStore } from '../context/LanguageContext'
+import ColorContext from '../context/ColorContext'
+import LanguageSelector from  './LanguageSelector'
+
+export default class App extends Component {
+
+    render() {
+        return (
+            <div className="ui container" >
+             <LanguageStore>
+              <LanguageSelector />
+                <ColorContext.Provider value="green">
+                      <UserCreate />
+                </ColorContext.Provider>
+            </LanguageStore>
+            </div>
+        )
+    }
+}
---
+++ src/components/Button.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import LanguageContext from '../context/LanguageContext'
+import ColorContext from '../context/ColorContext'
+
+
+export default class Button extends Component {
+
+        renderSubmit(language) {
+            return language === 'english' ? 'Submit' : 'Voorleggen' 
+        }
+
+        renderButton(color) {
+            return (
+                <button className={`ui button ${color}` }>
+                    <LanguageContext.Consumer>
+                        {({ language }) => this.renderSubmit(language) }
+                    </LanguageContext.Consumer>
+                </button>
+            );
+        }
+
+    render() {
+        return (
+         <ColorContext.Consumer>
+            {(color) => this.renderButton(color) }
+        
+        </ColorContext.Consumer>
+        )
+    }
+}
